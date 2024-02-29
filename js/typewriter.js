45b34a4077f69efcdd1a6d997e6816a66e505c0b
HariNivasan C <120179086+harinivasanc2000@users.noreply.github.com>
2024-02-29 20:26:11 +0000

    Add files via upload

---
+++ js/typewriter.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+var typed = new Typed('#element', {
+        strings: ['Software Developer.'],
+        typeSpeed: 40,
+        loop: true,
+        loopCount: Infinity,
+
+        fadeOut: false,
+        fadeOutClass: 'typed-fade-out',
+        fadeOutDelay: 800,
+
+        //backDelay: 700,
+        //backspeed: 1200,
+
+        //smartBackspace: true,
+
+
+
+
+
+    })
+        ;
+
+
+
